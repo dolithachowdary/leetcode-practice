d5d0893f3bf7a54741bf680e62b039e4d7844673
Dolitha Chowdary <155249751+dolithachowdary@users.noreply.github.com>
2025-11-04 21:32:17 +0530

    Time: 22 ms (78.18%), Space: 18 MB (38.7%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,80 +1,21 @@
-import collections
-from sortedcontainers import SortedList   # Requires installation of sortedcontainers
-
 class Solution:
-    def findXSum(self, nums: list[int], k: int, x: int) -> list[int]:
-        cnt = collections.Counter()
-        # Each element in the sets is (frequency, value)
-        top = SortedList()
-        bot = SortedList()
-        current_sum = 0
-        n = len(nums)
-        ans = []
-        
-        def _remove(val):
-            nonlocal current_sum
-            f = cnt[val]
-            if f == 0:
-                return
-            key = (f, val)
-            # Remove from whichever set it belongs
-            if key in top:
-                top.remove(key)
-                current_sum -= f * val
-            else:
-                bot.remove(key)
-        
-        def _add(val):
-            nonlocal current_sum
-            f = cnt[val]
-            if f == 0:
-                return
-            key = (f, val)
-            # decide if it goes to top or bot
-            if len(top) < x:
-                top.add(key)
-                current_sum += f * val
-            else:
-                # compare with smallest in top; if key is "better", swap
-                if top and key > top[0]:
-                    # move smallest top to bot
-                    smallest_top = top.pop(0)
-                    current_sum -= smallest_top[0] * smallest_top[1]
-                    bot.add(smallest_top)
-                    # put new key in top
-                    top.add(key)
-                    current_sum += f * val
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        res = []
+        def count(arr):
+            hm = {}
+            for i in arr:
+                if i in hm:
+                    hm[i]+=1
                 else:
-                    bot.add(key)
-        
-        for i, v in enumerate(nums):
-            # 1) prepare to add v
-            _remove(v)
-            cnt[v] += 1
-            _add(v)
-            
-            # window start index
-            if i >= k:
-                old = nums[i - k]
-                _remove(old)
-                cnt[old] -= 1
-                _add(old)
-            
-            if i >= k - 1:
-                # ensure top has exactly x (or fewer if distinct < x)
-                while len(top) < x and bot:
-                    key = bot.pop()   # largest in bot
-                    top.add(key)
-                    current_sum += key[0] * key[1]
-                # ensure no element in bot is "better" than any in top
-                while bot and top and bot[-1] > top[0]:
-                    keyB = bot.pop()
-                    keyT = top.pop(0)
-                    current_sum -= keyT[0] * keyT[1]
-                    current_sum += keyB[0] * keyB[1]
-                    bot.add(keyT)
-                    top.add(keyB)
-                
-                ans.append(current_sum)
-        
-        return ans
+                    hm[i] = 1
+            return hm
+
+        for i in range(len(nums)-k+1):
+            hm = count(nums[i:i+k])
+            sorted_items = sorted(hm.items(), key=lambda p: (-p[1], -p[0]))
+
+            total = 0
+            for num,freq in sorted_items[:x]:
+                total += num*freq
+            res.append(total)
+        return res
