ac6e94e9aa1fb03b73bf32e0ab7ba4e25d38d369
Dolitha Chowdary <155249751+dolithachowdary@users.noreply.github.com>
2026-02-05 09:22:59 +0530

    Time: 58 ms (65.92%), Space: 19.4 MB (10.76%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -1,5 +1,12 @@
 class Solution:
-  def constructTransformedArray(self, nums: list[int]) -> list[int]:
-    n = len(nums)
-    return [nums[(i + num % n + n) % n]
-            for i, num in enumerate(nums)]
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        res = []
+        length = len(nums)
+        for index, value in enumerate(nums):
+            if value > 0:
+                res.append(nums[(index+value) % length])
+            elif value < 0:
+                res.append(nums[(index - abs(value) + length) % length])
+            else:
+                res.append(0)
+        return res
